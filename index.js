798c454c4a0c7a145cb5b121ad183250b385db2c
Nick Landas <nick.landas@gmail.com>
2022-10-31 21:09:25 -0500

    commit tree 109fc076b514b4e563faed0d9156683e8a76284f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+const mondayWork = function (action = "go to the office") {
+    return (`This Monday, I will ${action}.`);
+}
+mondayWork();
+
+const wrapAdjective = function (eee = '*') {
+return function attaboy (adj = 'really good'){
+    return `You are ${eee}${adj}${eee}!`
+}
+}
+wrapAdjective('!')
